d3b7bdbf685031789e6229492a6a8b7d6d274d44
Maksimilian <shelekhovmn@gmail.com>
2023-01-03 19:43:07 +0900

    Initial Commit

---
+++ Shape Builder ver.2/Shape_Builder_ver_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Shape_Builder_ver_2App.swift
+//  Shape Builder ver.2
+//
+//  Created by Maksimilian Shelekhov on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct Shape_Builder_ver_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
